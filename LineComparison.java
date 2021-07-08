be442d2470793dba6ae32f9232d56f4036c842c4
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-08 10:12:05 +0530

    calculating length of line

--- LineComparison.java
+++ LineComparison.java
@@ -1,8 +1,11 @@
+public class LineComparison{
 
-public class LineComparison {
-	public static void main(String[] args ) {
-		
-		System.out.println("Welcome To Line Comparision Program");
-		
+        public static void main(String args[]) {
+
+                System.out.println("Welcome To Line Comparision Program");
+                int X1=6,Y1=2,X2=2,Y2=8;
+                double distance = Math.sqrt((X2-X1)*(X2+X1) + (Y2-Y1) *(Y2+Y1)) ;
+                //System.out.println(distance);
+                System.out.println("Distance between "+"("+X1+","+Y1+"),"+"("+X2+","+Y2+") is "+distance);
 	}
 }
